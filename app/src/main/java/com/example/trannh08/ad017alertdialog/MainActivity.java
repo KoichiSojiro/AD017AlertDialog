b9aa62a9b86bf726d2f873b69904bcae096c7301
Koichi <koichisojiro@live.com>
2016-09-21 00:21:42 +0700

    initial

---
+++ app/src/main/java/com/example/trannh08/ad017alertdialog/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.trannh08.ad017alertdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
